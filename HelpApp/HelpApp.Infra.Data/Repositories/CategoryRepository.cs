4c7a10cff215c9c5029c5fc82db4efd1fe61f1dd
Geysa Ribeiro <geysa.ribeirotoppan@gmail.com>
2025-04-26 17:26:31 -0300

    Alterações em Category e Product Repository

--- HelpApp/HelpApp.Infra.Data/Repositories/CategoryRepository.cs
+++ HelpApp/HelpApp.Infra.Data/Repositories/CategoryRepository.cs
@@ -1,12 +1,107 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using Threading.Tasks;
 using System.Text;
-using System.Threading.Tasks;
+using Systems.Collections.Generic;
+using System.Linq;
+using HelpApp.Domain.Entities;
+using HelpApp.Domain.Interfaces;
+using HelpApp.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Net.NetworkInformation;
+
+
+
 
 namespace HelpApp.Infra.Data.Repositories
 {
-    public class CategoryRepository
+    public class CategoryRepository : ICategoryRepository
     {
+        #region Atributos
+
+        private readonly ApplicationDbContext_dbContext;
+
+        #endregion
+    
+        #region Construtor
+
+        public CategoryRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        #endregion
+
+        #region Metodos
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            var categories = await
+
+           _dbContext.Categories.AsNoTracking().OrderBy(Category => category.Name).ToListAsync();
+
+            return categories;
+        }
+
+        public async Task<Category> GetById(int id)
+        {
+            ValidateId(id);
+
+            var category = await _dbContext.Categories.FindAsync(id);
+
+            return category;
+
+        }
+
+        public async Task<Category> Create(Category category)
+        {
+            ValidateCategory(category);
+
+            _dbContext.Categories.Add(category);
+
+            await _dbContext.SaveChangesAsync();
+
+            return category;
+        }
+
+        public async Task<Category> Update(Category category)
+        {
+            ValidateCategory(category);
+
+            _dbContext.Categories.Update(category);
+
+            await _dbContext.SaveChangesAsync();
+
+            return category;
+        }
+
+        public async Task<Category> Remove(Category category)
+        {
+            ValidateCategory(category);
+
+            _dbContext.Categories.Remove(category);
+
+            await _dbContext.SaveChangesAsync();
+
+            return category;
+        }
+
+        #endregion
+
+        #region Validação
+
+        private void ValidateCategory(Category category)
+        {
+            if (category == null)
+                throw new ArgumentNullException(nameof(category), "Category must be provided.");
+        }
+
+        private void ValidateId (int id)
+        {
+            if (!id.HasValue || id <= 0)
+                throw new ArgumentException("Category ID has be a positive number.", nameof(id));
+        }
+
+
+        #endregion
     }
 }
--- HelpApp/HelpApp.Infra.Data/Repositories/ProductRepository.cs
+++ HelpApp/HelpApp.Infra.Data/Repositories/ProductRepository.cs
@@ -1,12 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Management.Instrumentation;
 using System.Text;
 using System.Threading.Tasks;
+using HelpApp.Domain.Entities;
+using HelpApp.Domain.Interfaces;
+using HelpApp.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace HelpApp.Infra.Data.Repositories
 {
-    public class ProductRepository
+    public class ProductRepository : IProductRepository
     {
+        #region Atributos
+
+        private readonly ApplicationDbContext _dbContext;
+
+        #endregion
+
+        #region Construtor
+
+        public ProductRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        #endregion
+
+        #region Metodos
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            var products = await _dbContext.Products.Include(products => product.Category).AsNoTracking().ToListAsync();
+
+            return products;
+        }
+
+        public async Task<Product> GetById(int id)
+        {
+            ValidateId(id);
+
+            var product = await _dbContext.Products.Include(product => product.Category).AsNoTracking().FirstOrDefaultAsync(product => product.Id == id);
+
+            return product;
+        }
+
+        public async Task<Product> Create(Product product)
+        {
+            ValidateProduct(product);
+
+            _dbContext.Products.Add(product);
+
+            await _dbContext.SaveChangeAsync();
+
+            return product;
+        }
+
+        public async Task<Product> Update (Product product)
+        {
+            ValidateProduct(product);
+
+            _dbContext.Products.Update(product);
+
+            await _dbContext.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<Product> Remove(Product product)
+        {
+            ValidateProduct(product);
+
+            _dbContext.Products.Remove(product);
+
+            await _dbContext.SaveChangesAsync();
+
+            return product;
+        }
+
+        #endregion
+
+        #region Validação
+
+        private void ValidateProduct(Product product)
+        {
+            if (product == null)
+                throw new ArgumentNullException(nameof(product), "Product must be provided.");
+        }
+
+        private void ValidateId(int id)
+        {
+            if (!id.HasValue || id <= 0)
+                throw new ArgumentException("Category ID has to be a positive number.", nameof(id));
+        }
+
+        #endregion
     }
 }
